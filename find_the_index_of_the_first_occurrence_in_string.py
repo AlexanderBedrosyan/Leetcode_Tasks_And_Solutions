c48b4044251824e330e18bcb0df6002c3bc4e9dc
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-12-21 01:16:51 +0200

    Added new solution for find the index of the first occurrence in string

---
+++ find_the_index_of_the_first_occurrence_in_string.py
@@ -0,0 +1,6 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+# or -1 if needle is not part of haystack.
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.index(needle) if needle in haystack else -1
