78ddc2028fa6eb0f97daa61a5ac1727a42d74822
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-03-08 12:01:25 +0200

    Add solution for Mid Evaluate Reverse Polish Notation

---
+++ mid_evaluate_reverse_polish_notation.py
@@ -0,0 +1,40 @@
+# You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+#
+# Evaluate the expression. Return an integer that represents the value of the expression.
+#
+# Note that:
+#
+# The valid operators are '+', '-', '*', and '/'.
+# Each operand may be an integer or another expression.
+# The division between two integers always truncates toward zero.
+# There will not be any division by zero.
+# The input represents a valid arithmetic expression in a reverse polish notation.
+# The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for token in tokens:
+            if token not in ('+', '-', '*', '/'):
+                stack.append(int(token))
+            else:
+                operand1 = stack.pop()
+                operand2 = stack.pop()
+                if token == '+':
+                    stack.append(operand2 + operand1)
+                elif token == '-':
+                    stack.append(operand2 - operand1)
+                elif token == '*':
+                    stack.append(operand2 * operand1)
+                elif token == '/':
+                    stack.append(int(operand2 * 1.0 / operand1))
+
+        return stack[-1]
+
+solve = Solution()
+print(solve.evalRPN(["4","13","5","/","+"]))
+print(solve.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
